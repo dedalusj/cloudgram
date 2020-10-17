75b09db3f1cd0dc4170c543f5ad46c9556d0e9c5
Jacopo <dedalusj@gmail.com>
2020-10-18 09:20:47 +1000

    Fixed coverage stats

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,23 @@
 module.exports = {
-    collectCoverage: true,
-    collectCoverageFrom: [
-        `src/**/*.js`,
-        '!**/(node_modules|dist|local)/**',
-        '!src/js/icons/**/*.js',
-    ],
-    coverageThreshold: {
-        global: {
-            branches: 90,
-            functions: 90,
-            lines: 80,
-            statements: 90,
-        },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    `src/**/*.js`,
+    '!**/(node_modules|dist|local)/**',
+    '!src/js/icons/**/*.js',
+    '!src/js/editor/**/*.js',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 80,
+      statements: 90,
     },
-    coverageReporters: ['json-summary', 'text', 'lcov'],
-    transform: {
-        "^.+\\.js$": "babel-jest",
-        ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2|svg)$": "jest-transform-stub"
-    },
-}
+  },
+  coverageReporters: ['json-summary', 'text', 'lcov'],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2|svg)$':
+      'jest-transform-stub',
+  },
+};
